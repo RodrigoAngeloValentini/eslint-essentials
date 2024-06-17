42d8cb15014a99dd70bf3041705b88e813f25127
Rodrigo Angelo Valentini <rodrigo.valentini@desbravador.com.br>
2024-06-17 15:19:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ESLint Essentials - Under Construction
+
+## Whats included?
+
+- Standard config base;
+- React plugin;
+- React Hooks plugin;
+- JSX a11y plugin;
+- Prettier;
---
+++ packages/react-native-js/index.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parser: "@babel/eslint-parser",
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  plugins: ["react", "react-native", "jsx-a11y", "import", "prettier"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-native/all",
+    "plugin:react-hooks/recommended",
+    "standard",
+    "prettier",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+    __DEV__: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "prettier/prettier": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/extensions": [".js", ".jsx"],
+  },
+  ignorePatterns: ["node_modules"],
+};
