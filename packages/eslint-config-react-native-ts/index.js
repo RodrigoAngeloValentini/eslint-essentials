f79bbde2d8440ba0fb9da07976ecfae608983b6b
Rodrigo Angelo Valentini <rodrigo.valentini@desbravador.com.br>
2024-07-30 08:34:51 -0300

    fix: add var rules

--- packages/eslint-config-react-native-ts/index.js
+++ packages/eslint-config-react-native-ts/index.js
@@ -51,6 +51,35 @@ module.exports = {
     ],
     'no-console': 'warn',
     'no-alert': 'warn',
+
+    'init-declarations': 'off',
+    'no-catch-shadow': 'off',
+    'no-delete-var': 'error',
+    'no-label-var': 'error',
+    'no-restricted-globals': [
+      'error',
+      {
+        name: 'isFinite',
+        message: 'Use Number.isFinite instead',
+      },
+      {
+        name: 'isNaN',
+        message: 'Use Number.isNaN instead',
+      },
+    ],
+    'no-shadow': 'error',
+    'no-shadow-restricted-names': 'error',
+    'no-undef': 'error',
+    'no-undef-init': 'error',
+    'no-undefined': 'off',
+    'no-unused-vars': [
+      'error',
+      {vars: 'all', args: 'after-used', ignoreRestSiblings: true},
+    ],
+    'no-use-before-define': [
+      'error',
+      {functions: true, classes: true, variables: true},
+    ],
   },
   settings: {
     react: {
